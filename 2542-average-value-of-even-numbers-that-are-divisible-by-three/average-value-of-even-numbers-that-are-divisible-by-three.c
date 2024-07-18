622d22f31bcdaeb1d707f86c0403b853502293b7
pratikshajamjar <146616699+pratikshajamjar@users.noreply.github.com>
2024-07-18 19:02:14 +0530

    Time: 5 ms (75.82%) | Memory: 8.6 MB (17.58%) - LeetSync

---
+++ 2542-average-value-of-even-numbers-that-are-divisible-by-three/average-value-of-even-numbers-that-are-divisible-by-three.c
@@ -0,0 +1,17 @@
+int averageValue(int* nums, int numsSize) {
+    int sum = 0;
+    int count = 0;
+
+    for (int i = 0; i < numsSize; i++) {
+        if (nums[i] % 2 == 0 && nums[i] % 3 == 0) {
+            sum += nums[i];
+            count++;
+        }
+    }
+
+    if (count == 0) {
+        return 0; 
+    }
+
+    return sum / count;
+}
