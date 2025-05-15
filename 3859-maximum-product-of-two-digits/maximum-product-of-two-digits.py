11d066d7b75da171ec161779d5c105644c55741d
pratikshajamjar <146616699+pratikshajamjar@users.noreply.github.com>
2025-05-15 20:17:08 +0530

    Time: 0 ms (100.00%) | Memory: 17.9 MB (39.69%) - LeetSync

--- 3859-maximum-product-of-two-digits/maximum-product-of-two-digits.py
+++ 3859-maximum-product-of-two-digits/maximum-product-of-two-digits.py
@@ -1,6 +1,10 @@
 class Solution:
     def maxProduct(self, n: int) -> int:
-
-       digit1, digit2 = nlargest(2, list(map(int, str(n))))
-
-       return digit1 * digit2   
+        # Convert the integer to a string to easily access each digit
+        s = str(n)
+        
+        # Sort the digits in ascending order
+        sorted_digits = sorted(s)
+        
+        # Extract the two largest digits (last two after sorting)
+        return (int(sorted_digits[-1]) * int(sorted_digits[-2]))
